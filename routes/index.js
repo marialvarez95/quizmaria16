cde85b78c92f88f378df6c1596314a90e3730fa7
Maria Alvarez de Sotomayor <mariaalvarezdesotomayor@MacBook-Pro-de-Maria-4.local>
2016-05-04 22:31:56 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController= require ('../controllers/quiz_controller');
+
 /* GET home page. */
 // crea la pagina index, con una sintaxis html que se define con express
 //las vistas EJS se general en el objeto de respuesta res
@@ -10,5 +12,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
   //render se aplica a la respuesta ( renderiza )
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',quizController.answer);
 // exportamos lo que hemos instalado
 module.exports = router;
